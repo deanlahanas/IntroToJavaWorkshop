cc84943cad665af9a5630fddf3fde33798f7a627
Shiva Malhotra <Shiva.malhotra.email@gmail.com>
2016-02-16 14:55:57 -0800

    I learned how to hide my robot.

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,19 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+Robot dean=new Robot();
+dean.setSpeed(10);
+dean.penDown();
+dean. move(200);
+dean.turn(90);
+dean.move(200);
+dean.turn(90);
+dean.move(200);
+dean.setSpeed(10);
 
-	
-		
-		
+for (int i = 0; i < 3; i++) {
+dean.turn(360/3);
+dean.move(200);	
+}
 	}
 }
